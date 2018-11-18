91e1d820403eee7c3b7fc537942dfb736ecdcb25
Miroslav Kisov <valsorim@abv.bg>
2018-11-18 17:18:27 +0200

    Fixing LoginPartial

---
+++ FastFoodWorkshop/FastFoodWorkshop.Models/FastFoodUser.cs
@@ -0,0 +1,14 @@
+﻿namespace FastFoodWorkshop.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System;
+
+    public class FastFoodUser : IdentityUser<int>
+    {
+        public string FullName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Address { get; set; }
+    }
+}
